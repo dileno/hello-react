b44f46698d8dbbcc808c2588c1c66b6dfa00434c
Martin S Ek <dileno@gmail.com>
2019-09-20 12:07:42 +0200

    added async and error handling

--- src/RandomUser.js
+++ src/RandomUser.js
@@ -5,38 +5,51 @@ class RandomUser extends Component {
         super();
         this.state = {
             user: [],
-            isLoading: false
+            isLoading: false,
+            error: null
         }
     }
 
     componentDidMount() {
         this.setState({ isLoading: true });
-        
-        fetch('https://randomuser.me/api/')
-        .then(results => {
-            return results.json();
-        })
-        .then(data => {
-            let user = data.results.map((user) => {
-                let userElm = '';
-                
-                if (typeof user !== undefined && typeof user.name !== undefined && typeof user.picture != undefined) {
-                    userElm = <div key={user}>
-                        <h2>{user.name.first} {user.name.last}</h2>
-                        <img src={user.picture.large} alt="" />
-                        </div>;
-                }
-                
-                return(userElm)
-            });
+        this.fetchRandomUser();        
+    }
 
-            this.setState({user: user, isLoading: false});
-        })
+    fetchRandomUser = async() => {
+        try {
+            await fetch('https://randomuser.me/api/')
+            .then(results => {
+                return results.json();
+            })
+            .then(data => {
+                let user = data.results.map((user) => {
+                    let userElm = '';
+                    
+                    if (typeof user !== undefined && typeof user.name !== undefined && typeof user.picture != undefined) {
+                        userElm = <div key={user}>
+                            <h2>{user.name.first} {user.name.last}</h2>
+                            <img src={user.picture.large} alt="" />
+                            </div>;
+                    }
+                    
+                    return(userElm)
+                });
+
+                this.setState({user: user, isLoading: false});
+            });
+        }
+        catch(error) {
+            this.setState({ error: error, isLoading: false });
+        }
     }
 
     render() {
-        const { user, isLoading } = this.state;
+        let { user, isLoading, error } = this.state;
 
+        if (error) {
+            return <p>{error.message}</p>;
+          }
+          
         if (isLoading) {
             return <p>Loading...</p>;
           }
